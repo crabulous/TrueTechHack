52b07b273220aac637677613cc5134a8a6764a6e
mcdogg17 <63260972+mcdogg17@users.noreply.github.com>
2023-03-29 19:37:43 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import json
-from flask import Flask, requests
+from flask import Flask, request, jsonify, Response
 import os
 from speech import speech_module
 from cut import cut_module
 from discription import discription_module
 
 app = Flask(__name__)
-
+app.config['JSON_AS_ASCII'] = False
 
 @app.route('/audio_description', methods=['POST'])
 def send_audio_description():
@@ -14,10 +14,17 @@ def send_audio_description():
     os.chdir('/home/cool/videos/')
     filename, _ = os.path.splitext(video_name)
     if os.path.isfile(filename + '.json'):
-        return json.load(open(filename + '.json'))
+        with open(filename + '.json', encoding='utf-8') as f:
+            data = json.load(f)
+            return data
     segments = speech_module.main(video_name)
     frames = cut_module.main(video_name, segments)
-    return discription_module.predict_step(frames)
+    print(segments)
+    disc = discription_module.predict_step(frames)
+    print(disc)
+    with open(f"{filename}.json", "w", encoding="utf-8") as file:
+        json.dump(disc, file, ensure_ascii=False)
+    return disc
 
 
 if __name__ == '__main__':
